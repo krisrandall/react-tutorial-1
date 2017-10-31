14091d5b01880a147a282baf83f71c0dec614c4d
krisrandall <kris.randall@gmail.com>
2017-10-31 20:56:30 +1000

    Part way through the tutorial in fact here : https://www.udemy.com/react-redux/learn/v4/t/lecture/4288086?start=0

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import React , { Component }    from 'react'
+import ReactDOM                 from 'react-dom'
+import YTSearch                 from 'youtube-api-search'
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+import SearchBar                from './components/search_bar'
+import VideoList                from './components/video_list'
+
+
+const YOUTUBE_API_KEY = 'AIzaSyClhT-yjW4mPtuQeJzq0kSz9W9QVNfwZl0'
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = { videos: [] }
+
+        YTSearch({ key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState( { videos } )
+        } )
+
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<App/>, document.querySelector('.container'))
+
+
