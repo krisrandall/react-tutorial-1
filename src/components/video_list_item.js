91eb8ba9fa0cc8448e5a62be417d88fb92f1aaae
krisrandall <kris.randall@gmail.com>
2017-11-02 08:47:07 +1000

    now the basic list and click and view works, but not yet the search

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,13 +1,13 @@
 
 import React    from 'react'
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
     console.log(video)
 
     const imageUrl = video.snippet.thumbnails.default.url
 
     return (
-        <li className="list-group-item">
+        <li className="list-group-item" onClick={() => onVideoSelect(video)}>
             <div className="video-list media">
 
                 <div className="media-left">
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import YTSearch                 from 'youtube-api-search'
 
 import SearchBar                from './components/search_bar'
 import VideoList                from './components/video_list'
-
+import VideoDetail              from './components/video_detail'
 
 const YOUTUBE_API_KEY = 'AIzaSyClhT-yjW4mPtuQeJzq0kSz9W9QVNfwZl0'
 
@@ -16,10 +16,16 @@ class App extends Component {
     constructor(props) {
         super(props)
 
-        this.state = { videos: [] }
+        this.state = { 
+            selectedVideo : null,
+            videos: [] 
+        }
 
         YTSearch({ key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState( { videos } )
+            this.setState( { 
+                videos: videos,
+                selectedVideo : videos[0]
+            } )
         } )
 
     }
@@ -28,7 +34,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         )
     }
